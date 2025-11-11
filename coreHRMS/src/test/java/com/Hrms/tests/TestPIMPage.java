941aa338b72c34bbfd78efd457b5c774462ccb62
appl <appl@192.168.1.74>
2025-11-11 13:57:05 +0530

    updated the code for searchEmployee

--- coreHRMS/src/test/java/com/Hrms/tests/TestPIMPage.java
+++ coreHRMS/src/test/java/com/Hrms/tests/TestPIMPage.java
@@ -65,7 +65,7 @@ public class TestPIMPage extends BasePage {
 
 	@Test(description = "Verify that the search functionality returns the correct employee", priority = 1)
 	public void searchEmployee() throws Exception {
-
+		loginPage.login();        //i am added this logic
 	}
 
 	@Test(description = "Verify that employee details can be edited successfully",groups= {"Smoke"}, priority = 2)
